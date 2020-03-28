3f00d389e7f0c93ac10c2623c2bf4595a6b44361
leanNiki <niklas.mirwani@lean-coders.at>
2020-03-28 21:26:39 +0100

    add new evaluator

--- src/chestcleaner/sorting/evaluator/EvaluatorType.java
+++ src/chestcleaner/sorting/evaluator/EvaluatorType.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public enum EvaluatorType {
 
-	BACK_BEGIN_STRING, BEGIN_BACK_STRING;
+	BACK_BEGIN_STRING, BEGIN_BACK_STRING, TWEAKED_CREATIVE;
 
 	/**
 	 * Returns the enum object if it's is equal to an existing entry.
@@ -21,14 +21,17 @@ public enum EvaluatorType {
 			return BACK_BEGIN_STRING;
 		} else if (str.equalsIgnoreCase(BEGIN_BACK_STRING.name())) {
 			return BEGIN_BACK_STRING;
+		} else if (str.equalsIgnoreCase(TWEAKED_CREATIVE.name())) {
+			return TWEAKED_CREATIVE;
 		}
 		return null;
 	}
 
 	public static List<String> getIDList() {
-		List<String> list = new ArrayList<String>();
+		List<String> list = new ArrayList<>();
 		list.add(BACK_BEGIN_STRING.name());
 		list.add(BEGIN_BACK_STRING.name());
+		list.add(TWEAKED_CREATIVE.name());
 		return list;
 	}
 
@@ -41,10 +44,12 @@ public enum EvaluatorType {
 			return new BackBeginStringEvaluator();
 		} else if (type.equals(BEGIN_BACK_STRING)) {
 			return new BeginBackStringEvaluator();
+		} else if (type.equals(TWEAKED_CREATIVE)) {
+			return new TweakedCreativeEvaluator();
 		}
 		return null;
 	}
 
-	public static EvaluatorType DEFAULT = EvaluatorType.BACK_BEGIN_STRING;
+	public static EvaluatorType DEFAULT = EvaluatorType.TWEAKED_CREATIVE;
 
 }
