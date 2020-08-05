cbfb6a6dcacee68967d78cd4070184f4084e7beb
tom2208 <tom-hauschild@outlook.de>
2020-08-06 00:08:53 +0200

    added permission of reset cmd to plugin.yml

--- src/chestcleaner/utils/InventoryConverter.java
+++ src/chestcleaner/utils/InventoryConverter.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import chestcleaner.config.PluginConfigManager;
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
